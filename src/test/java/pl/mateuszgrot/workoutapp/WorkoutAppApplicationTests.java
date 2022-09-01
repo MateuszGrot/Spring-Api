5ff8ea449f400897dd0011b8e06697b3576861d9
Mateusz Grot <mateuszgrot@gmail.com>
2022-09-01 12:13:17 +0200

    Create new project

---
+++ src/test/java/pl/mateuszgrot/workoutapp/WorkoutAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mateuszgrot.workoutapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
