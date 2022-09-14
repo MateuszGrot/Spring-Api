011a6d6a6b34352e5631ab0194de2afc62e87be7
Mateusz Grot <mateuszgrot@gmail.com>
2022-09-14 09:40:24 +0200

    Create mongodb

--- src/main/java/pl/mateuszgrot/workoutapp/domain/Workout.java
+++ src/main/java/pl/mateuszgrot/workoutapp/domain/Workout.java
@@ -1,5 +1,6 @@
 package pl.mateuszgrot.workoutapp.domain;
 
+import javax.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -7,13 +8,16 @@ import lombok.Setter;
 
 import java.time.Duration;
 import java.util.UUID;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Setter
 @Getter
+@Document
 public class Workout {
 
+  @Id
   private UUID id;
   private String name;
   private Duration duration;
